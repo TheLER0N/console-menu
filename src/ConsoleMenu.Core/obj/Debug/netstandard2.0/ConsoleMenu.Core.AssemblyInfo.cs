ed2ef261ac5e55b69bb5bb77aabd323074c5d54a
LERON <164945360+TheLER0N@users.noreply.github.com>
2026-09-13 02:11:35 +0300

    Initial commit: Console Menu MVP (.NET 4.8 + WPF)

--- src/ConsoleMenu.Core/obj/Debug/netstandard2.0/ConsoleMenu.Core.AssemblyInfo.cs
+++ src/ConsoleMenu.Core/obj/Debug/netstandard2.0/ConsoleMenu.Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleMenu.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+738894f73b3f2970b64bcefec57f689ad24411d2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1fabb62dcc9339db0e7e3e66f4200b54df23bac")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleMenu.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleMenu.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- src/ConsoleMenu.Wpf/Views/Ps4ShellView.xaml.cs
+++ src/ConsoleMenu.Wpf/Views/Ps4ShellView.xaml.cs
@@ -1,1 +1,23 @@
-﻿namespace ConsoleMenu.Wpf.Views { public partial class Ps4ShellView : ShellViewBase { public Ps4ShellView() { InitializeComponent(); } protected override string ThemeName => "ps4"; } }
+﻿using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace ConsoleMenu.Wpf.Views
+{
+    public partial class Ps4ShellView : ShellViewBase
+    {
+        public Ps4ShellView() { InitializeComponent(); }
+
+        protected override string ThemeName => "ps4";
+
+        protected override void UpdateHero()
+        {
+            base.UpdateHero();
+            var list = Find<ListBox>("GamesList");
+            var title = Find<TextBlock>("HeroTitle");
+            if (list == null || title == null) return;
+            var idx = list.SelectedIndex < 0 ? 0 : list.SelectedIndex;
+            title.RenderTransform = new TranslateTransform(idx * 150.0 + 178.0, 0);
+            if (list.SelectedItem != null) list.ScrollIntoView(list.SelectedItem);
+        }
+    }
+}
